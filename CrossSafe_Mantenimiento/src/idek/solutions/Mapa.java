ca95662b7834592ab48f2bb93348d8cb526c5cff
Izas <usabiaga.izaskun@gmail.com>
2012-04-23 14:21:06 +0200

    Añadido globo

--- CrossSafe_Mantenimiento/src/idek/solutions/Mapa.java
+++ CrossSafe_Mantenimiento/src/idek/solutions/Mapa.java
@@ -24,7 +24,7 @@ public class Mapa extends MapActivity{
 	    mapView = (MapView) findViewById(R.id.mapview);
 	    mapView.setBuiltInZoomControls(true);
 	    
-	    List<Overlay> mapOverlays = mapView.getOverlays();
+	   /* List<Overlay> mapOverlays = mapView.getOverlays();
 	    Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
 	    PosicionIncidencias itemizedoverlay = new PosicionIncidencias(drawable, this);
 	    
@@ -32,8 +32,26 @@ public class Mapa extends MapActivity{
 	    OverlayItem overlayitem = new OverlayItem(point, "Kaixo EH!", "Idek Solutions se encuentra en EH");
 	    
 	    itemizedoverlay.addOverlay(overlayitem);
-	    mapOverlays.add(itemizedoverlay);
+	    mapOverlays.add(itemizedoverlay);*/
 	    
+	    List<Overlay> mapOverlays = mapView.getOverlays();
+		
+		// first overlay
+		Drawable drawable = getResources().getDrawable(R.drawable.marker);
+		SimpleItemizedOverlay itemizedOverlay = new SimpleItemizedOverlay(drawable, mapView);
+		
+		GeoPoint point = new GeoPoint((int)(51.5174723*1E6),(int)(-0.0899537*1E6));
+		OverlayItem overlayItem = new OverlayItem(point, "Tomorrow Never Dies (1997)", 
+				"(M gives Bond his mission in Daimler car)");
+		itemizedOverlay.addOverlay(overlayItem);
+		
+		GeoPoint point2 = new GeoPoint((int)(51.515259*1E6),(int)(-0.086623*1E6));
+		OverlayItem overlayItem2 = new OverlayItem(point2, "GoldenEye (1995)", 
+				"(Interiors Russian defence ministry council chambers in St Petersburg)");		
+		itemizedOverlay.addOverlay(overlayItem2);
+		
+		mapOverlays.add(itemizedOverlay);
+		
 	}
 	
 	@Override
